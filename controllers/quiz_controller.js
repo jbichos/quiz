ad396b185d949aa40e59ffa7980a7c29fc8ee878
jbichos <jbichos@hotmail.com>
2015-08-03 10:34:12 +0200

    Editar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -33,7 +33,7 @@ if (!req.query.search) {
 // GET /quizes/:id
 exports.show = function(req, res) {
   res.render('quizes/show', { quiz: req.quiz, errors: []});
-};
+};  // req.quiz: instancia de quiz cargada con autoload
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
@@ -56,13 +56,37 @@ exports.new = function(req, res) {
 exports.create = function(req, res){
   var quiz = models.Quiz.build( req.body.quiz );
   var errors = quiz.validate();//ya que el objeto errors no tiene then(
-    if (errors){
+  if (errors){
       var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
       for (var prop in errors) errores[i++]={message: errors[prop]};
-      res.render('quizes/new', {quiz: quiz, errors: errores});
-    } else {
+        res.render('quizes/new', {quiz: quiz, errors: errores});
+  } else {
       // save: guarda en DB campos pregunta y respuesta de quiz
       quiz.save({fields: ["pregunta", "respuesta"]})
       .then( function(){ res.redirect('/quizes')}) ;
-    }
+  }
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  errors = req.quiz.validate();
+  if (errors){
+    var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+    for (var prop in errors) errores[i++]={message: errors[prop]};
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+  } else {
+    // save: guarda campos pregunta y respuesta en DB
+    req.quiz.save( {fields: ["pregunta", "respuesta"]})
+    .then( function(){ res.redirect('/quizes')});
+  }     // Redirección HTTP a lista de preguntas (URL relativo)
 };
